ce94b1479e81a14632ec1d3b2c4e50f556b1d146
Spmaurya689 <spmaurya689@gmail.com>
2023-01-27 14:07:08 +0000

    print multiple lines

---
+++ hello.java
@@ -0,0 +1,8 @@
+class hello{
+    public static void main(String args[]){
+        System.out.println("Hello sp maurya your chilhood love is Roze\n");
+        System.out.println("Hello sp maurya your chilhood love is Roze\n");
+        System.out.println("Hello sp maurya your chilhood love is Roze\n");
+        System.out.println("Hello sp maurya your chilhood love is Roze\n");
+    }
+}
